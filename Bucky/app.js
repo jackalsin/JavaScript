dc81a3ff3c7f47e6395b526c2f6c591c5521e6af
jackalsin <jackal.sin@hotmail.com>
2016-06-08 14:41:22 -0400

    update module 5

--- Bucky/app.js
+++ Bucky/app.js
@@ -10,3 +10,23 @@ var person = {
 };
 
 console.log(person);
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log(addNumbers(1, 3));
+
+function worthless() {
+/* undefined */
+}
+console.log(worthless());
+
+/* anonymous function */
+var printBacon = function () {
+    console.log("bacon is healthy, don't believe the doctors!");
+};
+
+printBacon();
+/* pass a function, call this code in 5 seconds */
+setTimeout(printBacon, 5000);
