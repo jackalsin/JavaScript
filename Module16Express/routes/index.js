594a24367826d4523013e62340bd8052b9d71d64
jackalsin <jackal.sin@hotmail.com>
2016-06-09 10:17:13 -0400

    overnight backup

--- Module16Express/routes/index.js
+++ Module16Express/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    /* This is passing a parameters. */
+    name: 'Bucky'
+  });
 });
 
 module.exports = router;
