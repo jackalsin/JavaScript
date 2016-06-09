4fa46051ba150cc07c2b1e7c8f7c307e2f5dc50d
jackalsin <jackal.sin@hotmail.com>
2016-06-08 20:46:21 -0400

    send index.html

--- Module13CreateAnBasicSever/public/sever.js
+++ Module13CreateAnBasicSever/public/sever.js
@@ -2,15 +2,28 @@
  * Created by jacka on 6/8/2016.
  */
 var http = require('http');
+var fs = require('fs');
+
+/**
+ * 404 response
+ */
+function send404Response(response) {
+    response.writeHead(404, {"Context-Type": "text/plain"});
+    response.write("Error 404: Page not found");
+    response.end();
+}
 
 /**
  * The request is what a user sends,
- * while the response is what we send back */
+ * while the response is what we send back
+ */
 function onRequest(request, response) {
-    console.log("A user made a request " + request.url);
-    response.writeHead(200, {"Context-Type" :"text/plain"});
-    response.write("Here is your response");
-    response.end();
+    if (request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, {"Context-Type": "text/html"});
+        fs.createReadStream("./index.html").pipe(response);
+    } else {
+        send404Response(response);
+    }
 }
 
 http.createServer(onRequest).listen(8888);
