20034f796941734678b038d81dde66a4790c8e76
jackalsin <jackal.sin@hotmail.com>
2016-07-07 09:09:22 -0400

    solve-2

--- Cousera/Week1Exercise/node-examples/rectangle-2.js
+++ Cousera/Week1Exercise/node-examples/rectangle-2.js
@@ -1,3 +1,23 @@
 /**
  * Created by jacka on 7/6/2016.
  */
+module.exports = function (x, y, callback) {
+    try {
+        if (x <= 0 || y <= 0) {
+            throw new Error("rectangle dimensions should be greater" +
+                " than zero: l = " + x + ", and b = " + y);
+        } else {
+            // set error to null, the first one is error parameter
+            callback(null, {
+                perimeter: function () {
+                    return 2 * (x + y);
+                },
+                area: function () {
+                    return x * y;
+                }
+            });
+        }
+    } catch (error) {
+        callback(error, null);
+    }
+};
