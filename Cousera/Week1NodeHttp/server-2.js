d027b9483fd6db456e0fd1dcf23851c80cb88617
jackalsin <jackal.sin@hotmail.com>
2016-07-07 21:03:50 -0400

    Fixed Bug uncheck file existence

--- Cousera/Week1NodeHttp/server-2.js
+++ Cousera/Week1NodeHttp/server-2.js
@@ -23,10 +23,17 @@ var server = http.createServer(function (req, res) {
         
         if (fileExtension == '.html') {
             fs.exists(filePath, function(exists) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end("<h1>Error 404: " + fileUrl + " not found</h1>");
-                return;
-            });
+                if (!exists) {
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    res.end("<h1>Error 404: " + fileUrl + " not found</h1>");
+                    return;
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                /* It reads the file and pipe to the resource */
+                fs.createReadStream(filePath).pipe(res);
+                }
+
+            );
         } else {
             res.writeHead(404, {'Content-Type': 'text/html'});
             res.end('<h1>Error 404: ' + req.method + " not supported</h1>");
