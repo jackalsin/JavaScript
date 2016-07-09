89e1305e5a16368faabf1fb90a223c6871936179
jackalsin <jackal.sin@hotmail.com>
2016-07-08 20:53:10 -0400

    Fix View Engine

---
+++ Cousera/Week1Ex5Express/routes/dishRouter.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  /**
+   * This is render contains two parameters
+   * @index index template defined in './views'
+   * @param: parameters used in ejs
+   */
+  res.render('dishes', { title: 'Dishes' });
+});
+
+module.exports = router;
