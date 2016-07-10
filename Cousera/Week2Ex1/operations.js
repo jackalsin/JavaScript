d3e9ab729c283221b7906ee34f81b2d0df974ea3
jackalsin <jackal.sin@hotmail.com>
2016-07-09 20:31:15 -0400

    update mg

--- Cousera/Week2Ex1/operations.js
+++ Cousera/Week2Ex1/operations.js
@@ -1,3 +1,58 @@
 /**
  * Created by jacka on 7/9/2016.
  */
+var assert = require('assert');
+/**
+ * @param db: database
+ * @param document: document to insert
+ * @param collection: collection to insert to
+ * @param callback: call back function
+ */
+exports.insertDocument = function(db, document, collection, callback) {
+  // Get the documents collection
+  var coll = db.collection(collection);
+  coll.insert(document, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted " + result.result.n + " documents into the document collection "
+      + collection);
+    callback(result);
+  });
+};
+
+exports.findDocuments = function(db, collection, callback) {
+  // Get the documents collection
+  var coll = db.collection(collection);
+  //find some documents
+  coll.find({}).toArray(function (err, docs) {
+    assert.equal(err, null);
+    callback(docs);
+  });
+};
+
+exports.removeDocument = function(db, document, collection, callback) {
+
+  // Get the documents collection
+  var coll = db.collection(collection);
+
+  // Delete the document
+  coll.deleteOne(document, function(err, result) {
+    assert.equal(err, null);
+    console.log("Removed the document " + document);
+    callback(result);
+  });
+};
+
+exports.updateDocument = function(db, document, update, collection, callback) {
+
+  // Get the documents collection
+  var coll = db.collection(collection);
+
+  // Update document
+  coll.updateOne(document
+    , { $set: update }, null, function(err, result) {
+
+      assert.equal(err, null);
+      console.log("Updated the document with " + update);
+      callback(result);
+    });
+};
--- Cousera/Week2Ex1/server.js
+++ Cousera/Week2Ex1/server.js
@@ -1,3 +1,34 @@
 /**
  * Created by jacka on 7/9/2016.
  */
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+var dboper  = require('./operations');
+
+var url = 'mongodb://localhost:27017/conFusion';
+// Connect URL
+MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err);
+  console.log("Connect correctly to server");
+
+  dboper.insertDocument(db, {name:"Vadonut", description: "Test"},
+  "dishes", function(result) {
+      console.log(result.ops);
+
+      dboper.findDocuments(db, 'dishes', function(docs) {
+        console.log(docs);
+        dboper.updateDocument(db, {name:'Vadonut'}, {description:"Updated Test"},
+        "dishes", function (result) {
+            console.log(result.result);
+            dboper.findDocuments(db, "dishes", function (docs) {
+              console.log(docs);
+              // db.dropCollection("dishes", function (result) {
+              //   console.log("Result = " + result);
+              //
+              //   db.close();
+              // });
+            });
+          });
+      });
+    });
+});
